cc5aba112e312ea52c76a6af399876cba5e47e03
CodeRunner <44100023+peakupcoderunner@users.noreply.github.com>
2018-10-26 23:57:50 +0300

    Add files via upload

--- CodeRunner.Warp6.PeopleFunctions/Function.cs
+++ CodeRunner.Warp6.PeopleFunctions/Function.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -23,5 +26,39 @@ namespace CodeRunner.Warp6.PeopleFunctions
         {
             return input?.ToUpper();
         }
+
+        public async Task<Person> InsertPerson(Person input, ILambdaContext context)
+        {
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            Table people = Table.LoadTable(client, "People");
+
+            Document pDocument = new Document();
+            pDocument["Id"] = input?.Id;
+            pDocument["Name"] = input?.Name;
+            pDocument["Surname"] = input?.Surname;
+
+            await people.PutItemAsync(pDocument);
+            return input;
+        }
+
+        public async Task<Person> FetchPerson(PersonFetchModel input, ILambdaContext context)
+        {
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            Table people = Table.LoadTable(client, "People");
+            var pDocument = await people.GetItemAsync(input?.Id);
+            return JsonConvert.DeserializeObject<Person>(pDocument.ToJson());
+        }
+    }
+
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class PersonFetchModel
+    {
+        public string Id { get; set; }
     }
 }
---
+++ CodeRunner.Warp6.Terminal/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CodeRunner.Warp6.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var uri = "https://ohfzo3bq31.execute-api.us-east-2.amazonaws.com/default";
+            var person = new { Id = "10", Name = "John", Surname = "Doe" };
+            using (HttpClient client = new HttpClient())
+            {
+                var putResponse = await client.PutAsync(uri, new StringContent(JsonConvert.SerializeObject(person)));
+                Console.WriteLine(await putResponse.Content.ReadAsStringAsync());
+                var postResponse = await client.PostAsync(uri, new StringContent(JsonConvert.SerializeObject(new { Id = person.Id })));
+                Console.WriteLine(await postResponse.Content.ReadAsStringAsync());
+            }
+            Console.ReadKey();
+        }
+    }
+}
